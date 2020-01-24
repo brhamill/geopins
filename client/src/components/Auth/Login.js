0f099a0cbff9c9a6b2b13e07df3a696e81603e0f
Brad Hamill <brad@hamill.com>
2020-01-24 12:39:53 -0700

    header and auth

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -18,6 +18,7 @@ const Login = ({ classes }) => {
       });
       const { me } = await client.request(ME_QUERY);
       dispatch({ type: 'LOGIN_USER', payload: me });
+      dispatch({ type: 'IS_LOGGED_IN', payload: googleUser.isSignedIn() });
     } catch (err) {
       onFailure(err);
     }
@@ -43,6 +44,7 @@ const Login = ({ classes }) => {
         onSuccess={onSuccess}
         onFailure={onFailure}
         isSignedIn={true}
+        buttonText='Login with Google'
         theme='dark'
       />
     </div>
