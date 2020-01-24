ab35b7e12f775700aed26849c1dd4c514e4fb610
Brad Hamill <brad@hamill.com>
2020-01-24 00:20:16 -0700

    google login

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+const { findOrCreateUser } = require('./controllers/userController');
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -10,12 +14,23 @@ mongoose
   .then(() => console.log('DB connected!'))
   .catch(err => console.error(err));
 
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
-
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        // find or create user
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.error(`Unable to authenticate user with token ${authToken}`);
+    }
+    return { currentUser };
+  }
 });
 
 server.listen().then(({ url }) => {
